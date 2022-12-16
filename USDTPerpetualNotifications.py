43a9ba6538e07ea71fdf18dada06a89db0f1da59
Mr.Framon <Mr.Framon@yandex.ru>
2022-12-17 03:00:30 +0400

    Made the information display in the program window

--- USDTPerpetualNotifications.py
+++ USDTPerpetualNotifications.py
@@ -1,26 +1,30 @@
 import sys
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtGui import QIcon
-from programWindow import Ui_MainWindow
 import time
 from datetime import datetime
-from pybit import usdt_perpetual
-from plyer import notification
 
-class HandleMessage(QtWidgets.QMainWindow):
-    
+from plyer import notification
+from pybit import usdt_perpetual
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtGui import QIcon
+
+from programWindow import Ui_MainWindow
+
+
+class HandleMessage(QtWidgets.QMainWindow):    
     def __init__(self, *args, **kwds):
         super(HandleMessage, self).__init__()
         self.programWindow = Ui_MainWindow()
         self.programWindow.setupUi(self)
         self.display()
         self.connect()
+        #self.addWindowProgram()
+        
 
     def display(self):
         self.programWindow.lineEdit.setPlaceholderText('Пример: btc, eth, doge')
 
+    #Подключение к API
     def connect(self):
-
         ws_linear = usdt_perpetual.WebSocket(
             test=False,
             ping_interval=3000,
@@ -29,16 +33,33 @@ class HandleMessage(QtWidgets.QMainWindow):
             )
 
         ws_linear.kline_stream(
-            callback=self.handle_message, 
+            callback=self.handleMessage, 
             symbol='BTCUSDT', 
             interval="1"
             )
+        
+    def handleMessage(self, message):
+        data = message['data'][0]
+        open = data["open"]
+        symbol = message["topic"][9:]
+        close = data['close']
+        turnover = '{0:,}'.format(int(float(data['turnover']))).replace(',', '.')
+        priceChange = round((close/open-1)*100, 2)
 
-    def handle_message(self, msg):
-        data = msg['data'][0]
-        close = data["close"]
-        print(f"Цена закрытия: {close}")
+        self.programWindow.lineEdit_2.setText(str(close))
+        self.programWindow.lineEdit_3.setText(symbol)
+        self.programWindow.lineEdit_4.setText(str(priceChange))
+        self.programWindow.lineEdit_5.setText(str(turnover))
 
+        
+
+        
+        
+        
+    #def addWindowProgram(self):
+        
+        
+        
 
 
 app = QtWidgets.QApplication([])
